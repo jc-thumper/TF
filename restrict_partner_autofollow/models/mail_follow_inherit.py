e0352d2e6d404503c8abead82a563db241ba197e
Vishnu M <vishnu.m@confianzit.biz>
2020-11-02 23:40:16 +0530

    [FIX] RESTRICT_PARTNER_AUTOFOLLOW: follower - inactive user issue

--- restrict_partner_autofollow/models/mail_follow_inherit.py
+++ restrict_partner_autofollow/models/mail_follow_inherit.py
@@ -8,8 +8,8 @@ class MailFollowers(models.Model):
 
     def _add_followers(self, res_model, res_ids, partner_ids, partner_subtypes, channel_ids, channel_subtypes, check_existing=False, existing_policy='skip'):
         if self._context.get('apply_mode', '') != 'direct':
-            users = self.env['res.users'].search([('partner_id', 'in', partner_ids)]).filtered(lambda rec: rec.has_group('base.group_user'))
-            partner_ids = users.mapped('partner_id').ids or []
+            users = self.env['res.users'].search([('partner_id', 'in', partner_ids), ('active', 'in', [True, False])])
+            partner_ids = users.filtered(lambda rec: rec.has_group('base.group_user')).mapped('partner_id').ids or []
 
         return super(MailFollowers, self)._add_followers(res_model, res_ids, partner_ids, partner_subtypes, channel_ids, channel_subtypes, check_existing, existing_policy)
 

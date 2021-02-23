f7b1f1318c43bc5373e7d25fdc98907fd9edf5c6
Vishnu M <vishnu.m@confianzit.biz>
2021-02-23 18:19:57 +0530

    [ADD] REPORT_EXTENSION

---
+++ report_extension/__manifest__.py
@@ -0,0 +1,21 @@
+
+{
+    'name': 'Report Extension',
+    'version': '12.0.1.0.0',
+    'summary': "Sale order and Invoice",
+    'category': 'Sale',
+    'author': 'Confianz',
+    'maintainer': 'Confianz',
+    'company': 'Confianz',
+    'website': 'https://www.confianzit.com',
+    'depends': [
+        'sale','account'
+    ],
+    'data': [
+        'views/report_template.xml',
+    ],
+    'license': "AGPL-3",
+    'installable': True,
+    'auto_install': False,
+    'auto_install': False,
+}

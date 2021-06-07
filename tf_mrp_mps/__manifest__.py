68479bbd72d2b7fed4d94932ebc263e7451d3ff5
tringuyen-novobi <nguyenminhtricpr@gmail.com>
2021-06-07 16:31:41 +0700

    [ADD][TF-25] Add import/export feature for MPS

---
+++ tf_mrp_mps/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'TF MPS',
+    'version': '1.0',
+    'website': 'https://www.novobi.com',
+    'category': '',
+    'author': 'Novobi LLC',
+    'depends': [
+        'mrp_mps',
+    ],
+    'description': '',
+    'data': [
+        'views/assets.xml',
+        'views/import_mps_views.xml',
+    ],
+    'images': [],
+    'demo': [],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'qweb': ['static/src/xml/*.xml'],
+}

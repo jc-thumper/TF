5010dcd58579dcc10fea7fab3d57bd15335479ae
tringuyen-novobi <tringuyen@novobi.com>
2021-06-09 18:07:31 +0700

    [ADD] Grouping RFQ

---
+++ tf_grouping/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'TF Grouping',
+    'version': '1.0',
+    'website': 'https://www.novobi.com',
+    'category': '',
+    'author': 'Novobi LLC',
+    'depends': [
+        'purchase',
+        'mrp'
+    ],
+    'description': '',
+    'data': [
+    ],
+    'images': [],
+    'demo': [],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'qweb': ['static/src/xml/*.xml'],
+}

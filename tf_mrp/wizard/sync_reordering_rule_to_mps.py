295023838804883f23189c10ea95a3e18475939f
tringuyen-novobi <tringuyen@novobi.com>
2021-06-14 11:22:50 +0700

    [IMP][TF-41] Change to search on db to process on the ram

--- tf_mrp/wizard/sync_reordering_rule_to_mps.py
+++ tf_mrp/wizard/sync_reordering_rule_to_mps.py
@@ -8,10 +8,13 @@ class SyncRRToMPS(models.Model):
     def run_sync(self):
         lot_stock_ids = self.env['stock.warehouse'].search([]).mapped('lot_stock_id')
         reordering_rules = self.env['stock.warehouse.orderpoint'].search([('location_id', 'in', lot_stock_ids.ids)])
+        product_ids = reordering_rules.mapped('product_id').ids
+        mp_schedules = self.env['mrp.production.schedule'].search([('product_id', 'in', product_ids)])
+
         for reordering_rule in reordering_rules:
-            mps = self.env['mrp.production.schedule'].search([('product_id', '=', reordering_rule.product_id.id)])
-            if mps:
-                mps.write({'forecast_target_qty': reordering_rule.safety_stock_target,
-                           'min_to_replenish_qty': reordering_rule.product_min_qty,
-                           'max_to_replenish_qty': reordering_rule.product_max_qty,
-                           })
+            for mps in mp_schedules:
+                if reordering_rule.product_id.id == mps.product_id.id:
+                    mps.write({'forecast_target_qty': reordering_rule.safety_stock_target,
+                               'min_to_replenish_qty': reordering_rule.product_min_qty,
+                               'max_to_replenish_qty': reordering_rule.product_max_qty,
+                               })

098af5bd252204bd34c8a44429b3942569264d01
nhutnguyen-novobi <nhut.nguyen@novobi.com>
2021-06-15 17:20:42 +0700

    [ADD][SI-842] Create the Product Forecast Config for all products in the MPS view

--- SI/mps_connector/__init__.py
+++ SI/mps_connector/__init__.py
@@ -8,4 +8,12 @@ from odoo import api, SUPERUSER_ID
 
 def setup_mps_connector(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    env['mrp.production.schedule'].sudo().init_forecast_result_from_mps_data()
+    mps_env = env['mrp.production.schedule'].sudo()
+
+    # Initial data from MPS data
+    demand_fore_data_dict = mps_env.get_demand_fore_data_dict()
+    # 1. Initialize the Forecast Result data
+    mps_env.init_forecast_result_from_mps_data(demand_fore_data_dict=demand_fore_data_dict)
+    # 2. Initialize the Product Forecast Configuration
+    mps_env.init_product_fore_config_from_mps_data(demand_fore_data_dict=demand_fore_data_dict)
+

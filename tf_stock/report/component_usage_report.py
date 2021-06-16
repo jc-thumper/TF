f5fd4b9b1f28f552fe5e397b7a6e2208c62895a6
tringuyen-novobi <tringuyen@novobi.com>
2021-06-16 11:11:29 +0700

    [ADD][TF-12] Add search for vendor

--- tf_stock/report/component_usage_report.py
+++ tf_stock/report/component_usage_report.py
@@ -5,5 +5,16 @@ class ComponentUsageReport(models.Model):
     _inherit = 'stock.move.line'
 
     categ_id = fields.Many2one('product.category', 'Product Category', related='product_id.categ_id', store=True)
-    # seller_ids = fields.Many2one('product.product', 'Product Vendor', related='product_id.seller_ids')
+    seller_id = fields.Many2one('product.product', 'Product Vendor', compute='_compute_seller_id', search='_search_seller')
     work_center_id = fields.Many2one('mrp.workcenter', 'Work Center', related='workorder_id.workcenter_id')
+
+    def _compute_seller_id(self):
+        for record in self:
+            record.seller_id = record.seller_ids[:1].id
+
+    def _search_seller(self, operator, value):
+        if operator == 'ilike':
+            smls = self.env['stock.move.line'].search([])
+            smls_ids = smls.filtered(lambda sml, key=value: sml.product_id.seller_ids[:1].display_name
+                                     and key.lower() in sml.product_id.seller_ids[:1].display_name.lower()).ids
+        return [('id', 'in', smls_ids)]

b1496e73b37ae498eb7468332f9a368ae7d1b6e7
tringuyen-novobi <tringuyen@novobi.com>
2021-06-18 15:14:41 +0700

    [IMP][TF-46] Change structure code

--- tf_mrp_mps/controllers/main.py
+++ tf_mrp_mps/controllers/main.py
@@ -8,28 +8,21 @@ from odoo.addons.web.controllers.main import ExcelExport, ExportXlsxWriter
 
 class ExcelExportInherit(ExcelExport, http.Controller):
     def from_data(self, fields, rows):
-        with ExportXlsxWriter(fields, len(rows)) as xlsx_writer:
-            results = []
-            try:
-                index = fields.index('Forecasted quantity at date/Date')
-                index_demand_forecast = fields.index('Forecasted quantity at date/Demand Forecast')
+        results = []
+        try:
+            index = fields.index('Forecasted quantity at date/Date')
+            index_demand_forecast = fields.index('Forecasted quantity at date/Demand Forecast')
 
-                for row_index, row in enumerate(rows):
-                    if not row[index_demand_forecast]:
-                        row[index_demand_forecast] = 0
-                    if row[index] and row[index] < datetime.datetime.today().date():
-                        for i in range(0, index):
-                            rows[row_index + 1][i] = row[i]
-                    else:
-                        results.append(row)
-            except ValueError:
-                pass
-
-            rows = results and results or rows
             for row_index, row in enumerate(rows):
-                for cell_index, cell_value in enumerate(row):
-                    if isinstance(cell_value, (list, tuple)):
-                        cell_value = pycompat.to_text(cell_value)
-                    xlsx_writer.write_cell(row_index + 1, cell_index, cell_value)
+                if not row[index_demand_forecast]:
+                    row[index_demand_forecast] = 0
+                if row[index] and row[index] < datetime.datetime.today().date():
+                    for i in range(0, index):
+                        rows[row_index + 1][i] = row[i]
+                else:
+                    results.append(row)
+        except ValueError:
+            pass
 
-        return xlsx_writer.value
+        rows = results and results or rows
+        return super().from_data(fields, rows)

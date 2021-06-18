487e711ed050cddc551790f71e979849398caf32
tringuyen-novobi <tringuyen@novobi.com>
2021-06-18 14:49:21 +0700

    [IMP][TF-46][TF-47] Auto create Line and export MPS line with current period forward

--- tf_mrp_mps/models/mrp_mps.py
+++ tf_mrp_mps/models/mrp_mps.py
@@ -1,3 +1,5 @@
+import datetime
+
 from odoo import api, fields, models, _
 
 import logging
@@ -18,3 +20,21 @@ class MrpProductionScheduleInherit(models.Model):
             'res_model': 'mrp.production.schedule',
         }
         return action
+
+    def get_production_schedule_view_state(self):
+        res = super(MrpProductionScheduleInherit, self).get_production_schedule_view_state()
+        Product_Forecast = self.env['mrp.product.forecast']
+        for production_schedule in res:
+            production_schedule_id = production_schedule.get('id')
+            forecasts = self.env['mrp.production.schedule'].browse(production_schedule_id).forecast_ids.filtered(lambda f: f.date > datetime.datetime.today().date())
+            dates = [f.date for f in forecasts]
+            forecast_values = production_schedule.get('forecast_ids')
+            for forecast in forecast_values:
+                if forecast.get('date_stop') not in dates:
+                    Product_Forecast.create({
+                        'forecast_qty': 0,
+                        'date': forecast.get('date_stop'),
+                        'replenish_qty': 0,
+                        'production_schedule_id': production_schedule_id,
+                    })
+        return res

dcc0901d2bcfaa02c55a91b332800b00e3b96fc2
tringuyen-novobi <74339543+tringuyen-novobi@users.noreply.github.com>
2021-06-22 11:06:51 +0700

    [ADD][TF-12] Add Component Usage and Production Report (#14)
    * [ADD][TF-12] Add Component Usage and Production Report
    
    * [ADD][TF-12] Add search for vendor
    
    * [IMP][TF-12] Remove redundant code
    
    * [IMP][TF-12] Change model name
    
    * [IMP][TF] Decrease the time when search vendor
    
    * [IMP][TF-12] Review code
    
    Co-authored-by: tringuyen-novobi <tringuyen@novobi.com>

---
+++ tf_stock/wizard/__init__.py
@@ -0,0 +1,2 @@
+from . import component_usage_wizard
+from . import production_report_wizard
---
+++ tf_stock/wizard/production_report_wizard.py
@@ -0,0 +1,32 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class ProductionWizard(models.TransientModel):
+    _name = "mrp.production.report.wizard"
+    _description = 'Open the popup to allow user choose start date and end date'
+
+    start_date = fields.Datetime(string='Start Date')
+    end_date = fields.Datetime(string='End Date')
+
+    def get_report(self):
+        self.ensure_one()
+        if self.start_date > self.end_date:
+            raise UserError(_('End date must be gather than Start Date.'))
+        tree_view_id = self.env.ref('tf_stock.mrp_production_report').id
+        domain = [('date', '<=', self.end_date), ('date', '>=', self.start_date)]
+        domain += [('reference', 'ilike', 'WH/MO')]
+        production_location = self.env['stock.location'].search([('name', 'ilike', 'Production')])
+        domain += [('location_id', '=', production_location.id)]
+        context = {'group_by': ['categ_id', 'product_id']}
+        action = {
+            'type': 'ir.actions.act_window',
+            'views': [(tree_view_id, 'tree')],
+            'view_mode': 'tree',
+            'name': _('Production Report (%s - %s)') % (self.start_date, self.end_date),
+            'res_model': 'stock.move.line',
+            'context': context,
+            'domain': domain,
+        }
+
+        return action

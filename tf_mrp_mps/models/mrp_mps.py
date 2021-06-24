a01554bcceaa856c6e222e4ba557c0494ea48db7
tringuyen-novobi <tringuyen@novobi.com>
2021-06-24 14:30:05 +0700

    [IMP][TF-46] Update logic

--- tf_mrp_mps/models/mrp_mps.py
+++ tf_mrp_mps/models/mrp_mps.py
@@ -1,5 +1,3 @@
-import datetime
-
 from odoo import api, fields, models, _
 
 import logging
@@ -8,6 +6,12 @@ _logger = logging.getLogger(__name__)
 
 class MrpProductionScheduleInherit(models.Model):
     _inherit = 'mrp.production.schedule'
+
+    future_forecast_ids = fields.Many2many('mrp.product.forecast', string='Forecasted quantity at date using Export', compute='_compute_future_forecast_ids')
+
+    def _compute_future_forecast_ids(self):
+        for record in self:
+            record.future_forecast_ids = [(6, 0, record.forecast_ids.filtered(lambda f: f.date > fields.Date.today()).ids)]
 
     @api.model
     def open_table(self):
@@ -23,17 +27,17 @@ class MrpProductionScheduleInherit(models.Model):
 
     def get_production_schedule_view_state(self):
         res = super(MrpProductionScheduleInherit, self).get_production_schedule_view_state()
-        Product_Forecast = self.env['mrp.product.forecast']
+        ProductForecast  = self.env['mrp.product.forecast']
         for production_schedule in res:
-            production_schedule_id = production_schedule.get('id')
-            forecasts = self.env['mrp.production.schedule'].browse(production_schedule_id).forecast_ids.filtered(lambda f: f.date > datetime.datetime.today().date())
-            dates = [f.date for f in forecasts]
-            forecast_values = production_schedule.get('forecast_ids')
-            for forecast in forecast_values:
-                if forecast.get('date_stop') not in dates:
-                    Product_Forecast.create({
+            production_schedule_id = production_schedule['id']
+            future_forecasts = self.env['mrp.production.schedule'].browse(production_schedule_id).forecast_ids.filtered(lambda f: f.date > fields.Date.today())
+            dates = [f.date for f in future_forecasts]
+            forecast_values_list = production_schedule['forecast_ids']
+            for forecast_vals in forecast_values_list:
+                if forecast_vals['date_stop'] not in dates:
+                    ProductForecast .create({
                         'forecast_qty': 0,
-                        'date': forecast.get('date_stop'),
+                        'date': forecast_vals['date_stop'],
                         'replenish_qty': 0,
                         'production_schedule_id': production_schedule_id,
                     })

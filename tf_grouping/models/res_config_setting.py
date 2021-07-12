6c0f17ad49c5b15df5aef5f452cb85e84e1baee4
tringuyen-novobi <74339543+tringuyen-novobi@users.noreply.github.com>
2021-07-12 14:32:31 +0700

    Feature/grouping rfq (#42)
    * [IMP] Change label name
    
    Co-authored-by: tringuyen-novobi <tringuyen@novobi.com>

--- tf_grouping/models/res_config_setting.py
+++ tf_grouping/models/res_config_setting.py
@@ -5,5 +5,4 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     allow_grouping_bom = fields.Boolean(string='Allow group RFQs by BOM', related='company_id.allow_grouping_bom', readonly=False)
-    vendor_ids = fields.Many2many('res.partner', string='Vendors', related='company_id.vendor_ids', readonly=False,
-                                  help='List of vendors who can group RFQs by BOM')
+    vendor_ids = fields.Many2many('res.partner', string='Vendors', related='company_id.vendor_ids', readonly=False)
